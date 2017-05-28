bd116b072e921e63161fee14c95145ee87dcb6b8
shoichiyamazaki <shoichiyamazaki@shoichiyamazakinoMacBook-Pro.local>
2017-05-28 13:18:13 +0900

    update location manager

--- Onigokko_game/ViewController.swift
+++ Onigokko_game/ViewController.swift
@@ -7,8 +7,9 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController ,CLLocationManagerDelegate{
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +19,37 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func locationGet(){
+        myLocationManager = CLLocationManager()
+        myLocationManager.delegate = self
+        
+        switch CLLocationManager.authorizationStatus() {
+        case .restricted:
+            print("no user")
+        case .denied:
+            print("no user")
+        case .notDetermined:
+            myLocationManager.requestWhenInUseAuthorization()
+        default:
+            print("peremited")
+        }
+        
+        if !CLLocationManager.locationServicesEnabled(){
+            return
+        }
+        myLocationManager.desiredAccuracy = kCLLocationAccuracyBest
+        myLocationManager.distanceFilter = kCLDistanceFilterNone
+        myLocationManager.requestLocation()
+    }
+    
+    
+    //ここでlocation更新されたら取得
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = manager.location else { return }
+        let lat = location.coordinate.latitude
+        let lng = location.coordinate.longitude
     }
 
 
