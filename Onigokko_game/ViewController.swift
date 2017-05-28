44a6cc74c0ad222b7f5f47ad0a4960e211d22e47
Taku Yamada <tmn.sby001@gmail.com>
2017-05-28 13:25:51 +0900

    Draw a pin

--- Onigokko_game/ViewController.swift
+++ Onigokko_game/ViewController.swift
@@ -7,19 +7,46 @@
 //
 
 import UIKit
+import GoogleMaps
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var googleMap: GMSMapView!
+    
+    //緯度経度 -> 金沢駅
+    let latitude: CLLocationDegrees = 36.5780574
+    let longitude: CLLocationDegrees = 136.6486596
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // ズームレベル.
+        let zoom: Float = 15
+        
+        // カメラを生成.
+        let camera: GMSCameraPosition = GMSCameraPosition.camera(withLatitude: latitude,longitude: longitude, zoom: zoom)
+        
+        // MapViewを生成.
+//        googleMap = GMSMapView(frame: CGRectMake(0, 0, self.view.bounds.width, self.view.bounds.height))
+        
+        // MapViewにカメラを追加.
+        googleMap.camera = camera
+        
+        //マーカーの作成
+        let marker: GMSMarker = GMSMarker()
+        marker.position = CLLocationCoordinate2DMake(latitude, longitude)
+        marker.map = googleMap
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    //CGRectMakeをwrappする関数
+    func CGRectMake(_ x: CGFloat, _ y: CGFloat, _ width: CGFloat, _ height: CGFloat) -> CGRect {
+        return CGRect(x: x, y: y, width: width, height: height)
+    }
 
 }
 
---
+++ Pods/Target Support Files/Pods-Onigokko_gameUITests/Pods-Onigokko_gameUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Onigokko_gameUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Onigokko_gameUITests
+@end
